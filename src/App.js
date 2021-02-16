758b7eaa2525794dd2dca03c19bd42eed6095308
sibin <asibin99@gmail.com>
2021-02-16 10:03:49 +0000

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css'
+import './styles/responsive.css'
+
+// import './styles/js/bootstrap.min'
+// import './styles/js/all'
+// import './styles/js/custom'
+// import './styles/js/modernizer'
+
+import Header from './components/header'
+import Banner from './components/banner'
+import About from './components/about'
+import TodaySpecial from './components/todaySpecial'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Header />
+  <Banner />
+  <About />
+  <TodaySpecial />
     </div>
   );
 }
---
+++ src/components/todaySpecial.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import i1 from '../styles/images/special-menu-1.jpg';
+import i2 from '../styles/images/special-menu-2.jpg';
+import i3 from '../styles/images/special-menu-3.jpg';
+
+class  TodaySpecial extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div className="special-menu pad-top-100 parallax">
+           
+        </div>
+         );
+    }
+}
+ 
+export default TodaySpecial;
